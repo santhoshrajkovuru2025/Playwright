c7dcb578d059315aa2af3cf4f6227d0f8ca41eea
santhoshrajkovuru2025 <srkovuru@gmail.com>
2026-04-09 12:26:41 +0530

    Added the Tsconfig.json and typescript

--- tests/handlingAlerts.spec.js
+++ tests/handlingAlerts.spec.js
@@ -1,33 +1,47 @@
 
-import {test,expect} from '@playwright/test'
+import { test, expect } from '@playwright/test'
 
-test('handling Alert Messages',async({browser})=>{
+test.describe('handling Alerts',() => {
 
-    const context =  await browser.newContext();
-    const page = await context.newPage();
+    test('handling Alert Messages', async ({ browser }) => {
 
-    await page.goto('https://rahulshettyacademy.com/AutomationPractice/');
-    // To Accept the Confirm Alert pop-up message.
-   // page.on('dialog',dialog=>dialog.accept());
-   // To Cancel the Confirm Alert pop-up message.
-   page.on('dialog',dialog=>dialog.dismiss());
-    await page.locator('#confirmbtn').click();
+        const context = await browser.newContext();
+        const page = await context.newPage();
 
+        await page.goto('https://rahulshettyacademy.com/AutomationPractice/');
+        // To Accept the Confirm Alert pop-up message.
+        // page.on('dialog',dialog=>dialog.accept());
+        // To Cancel the Confirm Alert pop-up message.
+        page.on('dialog', dialog => dialog.dismiss());
+        await page.locator('#confirmbtn').click();
+
+    });
+
+    test('Handling Alert messages',async({page})=>{
+
+        await page.goto('https://qa-practice.netlify.app/alerts');
+        page.on('dialog', async dialog=>{
+          await console.log(dialog.message())
+            dialog.accept();
+        })
+        await page.locator('#confirm-btn').click();
+        
+
+    });
+
+    test('Capture Screenshots and Visual Comparision', async ({ page }) => {
+
+
+        await page.goto('https://rahulshettyacademy.com/AutomationPractice/');
+        await expect(page.locator('#displayed-text')).toBeVisible();
+        // To take a screenshot for loactor or partially
+        await page.locator('#displayed-text').screenshot({ path: 'partialscreenshot.png' });
+        await page.locator('#hide-textbox').click();
+        // To take screenshot completely
+        await page.screenshot({ path: 'pagescreenshot.png' });
+        await expect(page.locator('#displayed-text')).toBeHidden();
+
+    });
 });
-
-test('Capture Screenshots and Visual Comparision', async({page})=>{
-
-
-    await page.goto('https://rahulshettyacademy.com/AutomationPractice/');
-    await expect(page.locator('#displayed-text')).toBeVisible();
-    // To take a screenshot for loactor or partially
-    await page.locator('#displayed-text').screenshot({path:'partialscreenshot.png'});
-    await page.locator('#hide-textbox').click();
-    // To take screenshot completely
-    await page.screenshot({path:'pagescreenshot.png'});
-    await expect(page.locator('#displayed-text')).toBeHidden();
-
-});
-
 
 
